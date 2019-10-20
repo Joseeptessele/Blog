318f8780001d7d6f1fffce5863764648b44c6428
Jose Pedron Tessele <josetesselep@gmail.com>
2019-10-20 00:54:24 -0300

    templates added

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+
+    {
+        'author': 'José Eduardo',
+        'title': 'Blog Post',
+        'content': 'First Post',
+        'date_posted': 'October 19, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post',
+        'content': 'Seconde Post',
+        'date_posted': 'October 20, 2019'
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {'title': 'About'})
